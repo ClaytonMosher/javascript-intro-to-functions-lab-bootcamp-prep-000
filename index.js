cfa9ce5b1badab992ffc2384f8bdbe64dccfaeca
clayton j mosher <mosherclayton@gmail.com>
2019-02-04 05:24:59 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,9 +4,17 @@ function logShout(string) {console.log(string.toUpperCase())}
 function logWhisper(string) {console.log(string.toLowerCase())}
 function sayHiToGrandma(string) {
   if (string.toLowerCase() === string) 
+<<<<<<< HEAD
     return "I can't hear you!"
   if (string.toUpperCase() === string) 
     return "YES INDEED!"
   if (string === "I love you, Grandma.")
     return "I love you, too."
   }
+=======
+    return "I can't hear you!"}
+  if (string.toUpperCase() === string)
+    return "YES INDEED!"}
+  if (string === "I love you, Grandma."){
+    return "I love you too."}
+>>>>>>> 727153e036759264051ae5ee40516c5861671040
